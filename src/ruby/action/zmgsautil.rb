6342c551bc585366fb2d05a078ad10045fa4f03d
Rohit Vyawahare <rohit.vyawahare@synacor.com>
2017-11-14 14:58:07 +0530

    Execute zmgsautil on mailbox node

--- src/ruby/action/zmgsautil.rb
+++ src/ruby/action/zmgsautil.rb
@@ -40,7 +40,7 @@ module Action # :nodoc
     attr :label, true
     def initialize(*arguments)
       super()
-      @runner = RunCommand.new(File.join(ZIMBRAPATH,'bin','zmgsautil'), ZIMBRAUSER, *arguments)
+      @runner = RunCommandOnMailbox.new(File.join(ZIMBRAPATH,'bin','zmgsautil'), ZIMBRAUSER, *arguments)
       @label = ''
       self.timeOut = 2400 #timeout to 40 minutes
     end
